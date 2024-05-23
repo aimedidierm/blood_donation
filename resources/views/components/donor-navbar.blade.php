49c27c3b2b592917bc690db2d3c2f4d25d080c2a
Aime Didier <aimedidiermugisha@gmail.com>
2024-05-23 17:16:46 +0300

    Chating system

--- resources/views/components/donor-navbar.blade.php
+++ resources/views/components/donor-navbar.blade.php
@@ -24,7 +24,7 @@
                 </a>
             </li>
             <li>
-                <a href="/chat"
+                <a href="/chatify"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <span class="material-symbols-outlined">
                         forum
